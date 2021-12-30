037118cf070e3757ffa1f54502d64571bf0e469c
Navis Rina <73897055+NaviusRina@users.noreply.github.com>
2021-12-31 00:50:50 +0300

    fixed
    Fixed: multiple character set, characters in empty input; Added: replacement of characters and numbers with new input

--- js/script.js
+++ js/script.js
@@ -1,94 +1,108 @@
-const input = document.querySelector(".calc-display");
-const locat = document.querySelector(".calc-operation");
-let express;
-let storageArray =  [JSON.parse(localStorage.getItem("equation"))];
-
+const IN_DISPLAY = document.querySelector(".calc-display");
+const IN_OPERATION_LIST = document.querySelector(".calc-operation");
+let calculationNote;
+let result;
+let inStorageArray = [JSON.parse(localStorage.getItem("equation"))];
 
 //отображение в дисплее
 function insertInDisplay(num) {
-  if (num === "№") {
-    express = input.textContent += num.replace(/№/gu, '\u221A');
+  if (num === '№') {
+    if (!IN_DISPLAY.textContent.match('\u221A')) {
+      calculationNote = IN_DISPLAY.textContent += num.replace(/№/g, '\u221A');
+    }
+  }
+  else if (isNaN(num)) {
+    if (num !== IN_DISPLAY.textContent[IN_DISPLAY.textContent.length-1] && IN_DISPLAY.textContent !== '') {
+      IN_DISPLAY.textContent += num;
+    }
+  }
+  else if (IN_DISPLAY.textContent == '0' && IN_DISPLAY.textContent !== '0.') {
+      calculationNote = IN_DISPLAY.textContent = num;
+  }
+  else if (IN_DISPLAY.textContent == result && !isNaN(num)) {
+    IN_DISPLAY.textContent = num;
   }
   else {
-    express = input.textContent += num;
+    calculationNote = IN_DISPLAY.textContent += num;
   }
 }
 
 //локальное хранилище
 function storageResults() {
-  storageArray.push(`${express} = ${input.textContent}`);
-  if (storageArray.length > 100) {
-    storageArray.shift();
+  inStorageArray.push(`${calculationNote} = ${IN_DISPLAY.textContent}`);
+  if (inStorageArray.length > 100) {
+    inStorageArray.shift();
   }
-  localStorage.setItem("equation", JSON.stringify(storageArray));
+  localStorage.setItem("equation", JSON.stringify(inStorageArray));
 }
+
+//добавление результатов ввода в дисплей и в список операций
+function outputResults() {
+  IN_DISPLAY.textContent = result;
+  IN_OPERATION_LIST.innerHTML += `${calculationNote} = ${result}` + `<br/>`;
+  storageResults();
+}
+
 
 //набор клавиатурой
 document.addEventListener("keydown", function (event) {
   if ((event.key).match(/[0-9\*\-\+\/]/)) {
-    express = input.textContent += event.key;
+    calculationNote = IN_DISPLAY.textContent += event.key;
   }
-  else if (input.textContent && (event.key).match(/Enter/)) {
-    let result = eval(input.textContent);
-    //
-    input.textContent = result;
-    locat.textContent += `${express} = ${result} \n`;
-    storageResults();
+  else if (IN_DISPLAY.textContent && (event.key).match(/Enter/)) {
+    result = eval(IN_DISPLAY.textContent);
+    outputResults();
   }
   else if ((event.key).match(/Delete/)) {
-    input.textContent = "";
+    IN_DISPLAY.textContent = "";
   }
 })
 
 //математические операции при нажатии на кнопку "="
 document.querySelector("#result-btn").addEventListener("click", function (event) {
   //возведение в степень
-  if (input.textContent.includes('^')) {
-    let arr = input.textContent.split('^');
-    input.textContent = Math.pow(arr[0], arr[1]);
-    locat.textContent += `${express} = ${Math.pow(arr[0], arr[1])} \n`;
+  if (IN_DISPLAY.textContent.includes('^')) {
+    let elem = IN_DISPLAY.textContent.split('^');
+    IN_DISPLAY.textContent = Math.pow(elem[0], elem[1]);
+    IN_OPERATION_LIST.innerHTML += `${calculationNote} = ${Math.pow(elem[0], elem[1])}` + `<br/>`;
     storageResults();
   }
   //квадратный корень
-  else if (input.textContent.includes('\u221A')) {
-    let x = input.textContent.replace(/\u221A/, "");
-    input.textContent = Math.sqrt(x);
-    locat.textContent += `${express} = ${Math.sqrt(x)} \n`;
+  else if (IN_DISPLAY.textContent.includes('\u221A')) {
+    let elem = IN_DISPLAY.textContent.replace(/\u221A/, "");
+    IN_DISPLAY.textContent = Math.sqrt(elem);
+    IN_OPERATION_LIST.innerHTML += `${calculationNote} = ${Math.sqrt(elem)}` + `<br/>`;
     storageResults();
   }
   //основные операции
   else {
-    let result = eval(input.textContent);
-    input.textContent = result;
-    locat.textContent += `${express} = ${result} \n`;
-    storageResults();
+    result = eval(IN_DISPLAY.textContent);
+    outputResults();
   }
 })
 
 //вычисление процентов
 document.querySelector("#percentage").addEventListener("click", function () {
-    let arr = input.textContent.split(/\D/g);
-    let x = arr[0];
-    let y = arr[1];
-    if (input.textContent.includes('*')) {
-      let result = x * (y / 100);
-      input.textContent = result;
-      locat.textContent += `${express}% = ${result} \n`;
-      storageResults();
+    let elem = IN_DISPLAY.textContent.split(/\D/g);
+    let x = elem[0];
+    let y = elem[1];
+    if (IN_DISPLAY.textContent.includes('*')) {
+      result = x * (y / 100);
+      outputResults();
+    } else if (IN_DISPLAY.textContent == '') {
+      IN_DISPLAY.textContent = '';
     } else {
-      let result = x / 100;
-      input.textContent = result;
-      locat.textContent += `${express}% = ${result} \n`;
-      storageResults();
+      result = x / 100;
+      outputResults();
     }
 })
 
 //вывод содержимого локального хранилища
 if (localStorage.getItem("equation")) {
-  locat.textContent = localStorage.getItem("equation").replace(/[,\"\[|\]]/g, " ");
+  IN_OPERATION_LIST.innerHTML = localStorage.getItem("equation").replace(/[a-z\"\[|\]]/g, " ").replace(/,/g, "<br />");
 }
 
 //очистка дисплея
-function clean() {
-  input.textContent = "";
+function cleanDisplay() {
+  IN_DISPLAY.textContent = "";
 }
