a8dd49abbf456f55305bf0edbe987331ecb4d900
Navis Rina <karya.09@mail.ru>
2024-10-18 20:27:01 +0300

    setting up plugins
    MiniCssExtractPlugin, postcss-loader.
    output a separate css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
   entry: {
         main: path.resolve(__dirname, './src/index.js'),
     }, // Точка входа для сборки проекта
-
   output: {
         path: path.resolve(__dirname, './dist'), // Путь для выходного файла сборки
         filename: '[name].bundle.js',// Имя выходного файла сборки
@@ -15,15 +16,40 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i, // Регулярное выражение для обработки файлов с расширением .css
-        use: [
+        test: /\.(sa|sc|c)ss$/, // Регулярное выражение для обработки файлов с расширением .css
+        // use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          // "style-loader",
           // Translates CSS into CommonJS
-          "css-loader",
+          // "css-loader",
           // Compiles Sass to CSS
+          // "sass-loader",
+        // ], // Загрузчики, используемые для обработки CSS-файлов
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                    },
+                  ],
+                ],
+              },
+            },
+          },
           "sass-loader",
-        ], // Загрузчики, используемые для обработки CSS-файлов
+        ],
+        // use: [
+        //   MiniCssExtractPlugin.loader,
+        //   "style-loader",
+        //   "css-loader",
+        //   "sass-loader",
+        // ],
       },
       {
           test: /\.js$/,
@@ -37,9 +63,14 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'webpack Boilerplate',
             template: path.resolve(__dirname, './src/index.html'), // шаблон
+            inject: 'body', //перенос в боди файла
             filename: 'index.html', // название выходного файла
         }),
         new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+          // filename: './[name].css',
+          // chunkFilename: './[name].css',
+        }),
     ],
 
   devServer: {
